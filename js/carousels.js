8a72420706f2fffc3bd12945bf313d0689da8105
Tinnitus <claudechow@proton.me>
2023-07-20 15:32:07 +0800

    Site updated: 2023-07-20 15:32:06

--- js/carousels.js
+++ js/carousels.js
@@ -1,8 +1,8 @@
-const colorBtn = document.getElementById("post-title");
+const colorBtn = document.getElementById("logo");
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 let currentColorIndex = 0;
 
 colorBtn.addEventListener("click", () => {
   currentColorIndex = (currentColorIndex + 1) % colors.length;
-  document.body.style.backgroundColor = colors[currentColorIndex];
+  document.body.style.background = colors[currentColorIndex];
 });
